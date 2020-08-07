b5346a6eda0df44ff64403a8dabf05e2b9387ed9
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2020-08-07 10:30:46 -0500

    update

--- app/views/lectures/api-money.js
+++ app/views/lectures/api-money.js
@@ -8,7 +8,7 @@ function fetchData() {
     })
     .then(data => {
       console.log(data.rates);
-      document.querySelector("#app").innerHTML = `<p> 1 US = ${data.rates.EUR} Euros</p>`
+      document.querySelector("#app").innerHTML = `<p> 1 US = ${data.rates.MXN.toFixed(2)} Pesos</p>`
     })
     .catch(error => {
       console.log(error);
