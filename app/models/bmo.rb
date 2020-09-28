20109e241e138c3ff2244986c467ca186a43447c
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2020-09-28 13:35:48 -0500

    update

--- app/models/bmo.rb
+++ app/models/bmo.rb
@@ -1,6 +1,6 @@
 class Bmo < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: true, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :name, presence: true
   validates :answer, presence: true
   validates :reason, presence: true
